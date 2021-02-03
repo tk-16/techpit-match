2e44393c464aae3f4b91828b3e7a2f8234b531dc
tk-16 <76659162+tk-16@users.noreply.github.com>
2021-02-03 19:32:27 +0900

    スワイプ機能　編集

--- app/javascript/src/swipe.js
+++ app/javascript/src/swipe.js
@@ -16,6 +16,12 @@ if(location.pathname == "/users") {
         card.style.transform = 'scale(' + (20 - index) / 20 + ') translateY(-' + 30 * index + 'px)';
         card.style.opacity = (10 - index) / 10;
       });
+
+      if (newCards.length == 0) {
+        $(".no-user").addClass("is-active");
+      }
+
+      
     }
 
     initCards();
@@ -67,6 +73,33 @@ if(location.pathname == "/users") {
       });
       
     });
+
+    function createButtonListener(reaction) {
+      let cards = document.querySelectorAll('.swipe--card:not(.removed)');
+
+  if (!cards.length) return false;
+
+  let moveOutWidth = document.body.clientWidth * 2;
+
+  let card = cards[0];
+  card.classList.add('removed');
+
+  if (reaction == "like") {
+    card.style.transform = 'translate(' + moveOutWidth + 'px, -100px) rotate(-30deg)';
+  } else {
+    card.style.transform = 'translate(-' + moveOutWidth + 'px, -100px) rotate(30deg)';
+  }
+
+  initCards();
+    }
+
+    $('#like').on('click', function() {
+      createButtonListener("like");
+    })
+
+    $('#dislike').on('click', function() {
+      createButtonListener("dislike");
+    })
 
   });
 
