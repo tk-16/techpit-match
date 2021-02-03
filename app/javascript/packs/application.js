137e585fc5140ca7ebc69b5813f5579618bdbe15
tk-16 <76659162+tk-16@users.noreply.github.com>
2021-02-03 19:22:45 +0900

    スワイプ機能の実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,8 @@ require("channels")
 
 require("src/profile_image_upload");
 
+require("src/swipe")
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
